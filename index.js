1d11883b200cba20cf9854b10bd916189dbb374c
mkbotoman <mkbotoman@gmail.com>
2015-04-22 20:26:22 -0700

    calling pInstaller from the test runner

--- index.js
+++ index.js
@@ -11,20 +11,20 @@ module.exports = exports = function(packages){
 exports.array = pInstaller
 
 function pInstaller(items, packages) {
-return;
+return console.log(items + " AND " + packages);
 }
 //helper function to parse array and 
 function parsedItems(arr){
-  var parsed = [];
-  var items = [];
-  var dependencies = [];
-  for ( l = 0; l < arr.length; l++ ) {
-  	var combine = arr[l];
-  	var split = combine.split(": ");
-  	items.push(split[0]);
-  	if(split[1])
-  		dependencies.push(split[1])
-  }
-return [items, dependencies];
+	var parsed = [];
+	var items = [];
+	var dependencies = [];
+  	for ( l = 0; l < arr.length; l++ ) {
+  		var combine = arr[l];
+  		var split = combine.split(": ");
+  		items.push(split[0]);
+  		if(split[1])
+  			dependencies.push(split[1])
+	}
+	return [items, dependencies];
 }
 
--- test.js
+++ test.js
@@ -7,12 +7,14 @@ var pInstaller = require('./index');
 function pInstallerTest() {
 	//happy path
 	var happyTest = [ "KittenService: ", "Leetmeme: Cyberportal", "Cyberportal: Ice", "CamelCaser: KittenService", "Fraudstream: Leetmeme", "Ice: " ];
+	var sorted = pInstaller(happyTest);	
+
 	//sad path
 	var sadTest = [ "KittenService: ", "Leetmeme: Cyberportal", "Cyberportal: Ice", "CamelCaser: KittenService", "Fraudstream: ", "Ice: Leetmeme", ];
 
 	//error path(?)
 
-	return console.log("yay!");
+	return console.log(sorted);
 }
 
-pInstallTest();
+pInstallerTest();
