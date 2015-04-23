3bd46f298010bd4c762e7b39368ed0c51d325157
mkbotoman <mkbotoman@gmail.com>
2015-04-22 20:17:31 -0700

    add happy function and start test layout

--- index.js
+++ index.js
@@ -4,3 +4,27 @@
  * @param {string} packages
  * @returns {Array}
  */
+module.exports = exports = function(packages){
+  return pInstaller(parsedItems(packages), packages)
+}
+
+exports.array = pInstaller
+
+function pInstaller(items, packages) {
+return;
+}
+//helper function to parse array and 
+function parsedItems(arr){
+  var parsed = [];
+  var items = [];
+  var dependencies = [];
+  for ( l = 0; l < arr.length; l++ ) {
+  	var combine = arr[l];
+  	var split = combine.split(": ");
+  	items.push(split[0]);
+  	if(split[1])
+  		dependencies.push(split[1])
+  }
+return [items, dependencies];
+}
+
--- test.js
+++ test.js
@@ -1,10 +1,18 @@
 /**
  * Function for testing
  **/
-function pInstallTest() {
-	//happy path
 
+var pInstaller = require('./index');
+
+function pInstallerTest() {
+	//happy path
+	var happyTest = [ "KittenService: ", "Leetmeme: Cyberportal", "Cyberportal: Ice", "CamelCaser: KittenService", "Fraudstream: Leetmeme", "Ice: " ];
 	//sad path
+	var sadTest = [ "KittenService: ", "Leetmeme: Cyberportal", "Cyberportal: Ice", "CamelCaser: KittenService", "Fraudstream: ", "Ice: Leetmeme", ];
 
 	//error path(?)
+
+	return console.log("yay!");
 }
+
+pInstallTest();
