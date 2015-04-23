69c4e61bcd04bb72405de4ddd38265594294680a
mkbotoman <mkbotoman@gmail.com>
2015-04-23 11:38:47 -0700

    trying to figure out why my happy path is unhappy

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
  * @param {string} packages
  * @returns {Array}
  */
-module.exports = exports = function(packages){
+module.exports = exports = function(packages) {
   return pInstaller(parsedItems(packages), packages)
 }
 
@@ -16,20 +16,30 @@ function pInstaller(items, packages) {
   var helper = [];
   var installMe = [];
 
-  for ( i = 0; i < packages.length; i++ ){
+  for ( i = 0; i < packages.length; i++ ) {
     var parsed = packages[i].split(": ");
     //check for circular dependency
-    if (parsed[1] && ) {
-      throw New Error("Packages contain a circular dependency!");
-      return;
-    }
-    // //if the dependent is not a dependency or already in the helper, add it to the helper
-    // if (helper.indexOf(parsed[0] > -1 && dependencies.indexOf(parsed[0] > -1) {
-    //   helper.push(parsed[0]);
+    // if (parsed[1] && ) {
+    //   throw New Error("Packages contain a circular dependency!");
+    //   return;
     // }
 
+    //if the dependent is not a dependency or already in the helper, add it to the helper
+    if (helper.indexOf(parsed[0]) === -1 && dependencies.indexOf(parsed[0]) === -1) {
+      helper.push(parsed[0]);
+    }
+    //if dependency is not already being installed
+    if (parsed[1] && installMe.indexOf(parsed[1]) === -1) {
+      installMe.push(parsed[1]);
+    }
   }
 
+  for (t = 0; t < helper.length; t++) {
+    if (installMe.indexOf(helper[t]) === -1) {
+      installMe.push(helper[t]);
+    }
+  }
+console.log(installMe);
   return installMe;
 }
 //helper function to parse array and output two equal length arrays
@@ -40,11 +50,11 @@ function parsedItems(arr){
   	for ( l = 0; l < arr.length; l++ ) {
   		var combine = arr[l];
   		var split = combine.split(": ");
+
   		items.push(split[0]);
+
   		if(split[1]) {
   			dependencies.push(split[1]);
-      } else {
-        dependencies.push("wait");
       }
 	 }
 	return [items, dependencies];
--- test.js
+++ test.js
@@ -8,9 +8,10 @@ function pInstallerTest() {
 	//happy path
 	var happyTest = [ "KittenService: ", "Leetmeme: Cyberportal", "Cyberportal: Ice", "CamelCaser: KittenService", "Fraudstream: Leetmeme", "Ice: " ];
 	var sorted = pInstaller(happyTest);	
-	var expected = ["Cyberportal", "Ice", "KittenService", "Leetmeme", "CamelCaser"]
-	console.log(expected == sorted);
-	
+console.log(sorted);
+	var expected = ['Cyberportal','Ice','KittenService','Leetmeme','CamelCaser','Fraudstream' ]
+	console.log(expected === sorted);
+
 	//sad path
 	var sadTest = [ "KittenService: ", "Leetmeme: Cyberportal", "Cyberportal: Ice", "CamelCaser: KittenService", "Fraudstream: ", "Ice: Leetmeme", ];
 
